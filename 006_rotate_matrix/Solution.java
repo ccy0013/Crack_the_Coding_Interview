122b775bc88e38e25abc43a42071eda1f57b9fa3
ccy0013 <ccy604080969@gmail.com>
2015-06-12 22:50:36 -0700

    Add method 3, a single-pass method with constant space

--- 006_rotate_matrix/Solution.java
+++ 006_rotate_matrix/Solution.java
@@ -6,7 +6,10 @@
  ************************************************************************/
 
 public class Solution{
+
+	//For N*N image
 	
+	//Method 1: Single-pass solution with O(N^2) space
 	public static Pixel[][] rotateMatrix( Pixel[][] matrix ){
 
 		int size = matrix.length;
@@ -21,6 +24,7 @@ public class Solution{
 		return copy;
 	}
 
+	//Method 2: Two-pass solution with O(1) space
 	public static void rotateMatrix2( Pixel[][] matrix ){
 		Pixel temp = new Pixel();
 		for( int i=0; i<matrix.length; i++ ){
@@ -39,6 +43,25 @@ public class Solution{
 			}
 		}
 	}
+
+	//Method 3: Single-pass solution with O(1) space
+	public static void rotateMatrix3( Pixel[][] matrix ){
+		Pixel temp = new Pixel();
+
+		int size = matrix.length;
+
+		for( int layer=0; layer<size/2; layer++ ){
+			for( int j=layer; j<size-layer-1; j++ ){
+				temp = matrix[j][size-layer-1];
+
+				matrix[j][size-layer-1] = matrix[layer][j];
+				matrix[layer][j] = matrix[size-j-1][layer];
+				matrix[size-j-1][layer] = matrix[size-layer-1][size-j-1];
+				matrix[size-layer-1][size-j-1] = temp;
+			}
+		}
+	}
+
 	public static void main( String[] args ){
 
 		int size = 5;
@@ -51,7 +74,7 @@ public class Solution{
 			}
 		}
 
-		Pixel[][] result = new Pixel[3][3];
+		Pixel[][] result = new Pixel[size][size];
 
 		result = rotateMatrix( matrix );
 
@@ -69,18 +92,40 @@ public class Solution{
 		System.out.println( "[Matrix Rotated by Method 1]");
 		
 		for( int i=0; i<result.length; i++ ){
-			for( int j=0; j<result.length; j++ ){
+			for( int j=0; j<result[0].length; j++ ){
 				System.out.print( Character.toString(result[i][j].red) + Character.toString(result[i][j].green) + " " );
 			
 				if( j==result[0].length-1 )
 					System.out.println( ' ' );
 			}
 		}
-
+		
 		rotateMatrix2( matrix );
 		
 		System.out.println( "" );
 		System.out.println( "[Matrix Rotated by Method 2]");
+
+		for( int i=0; i<matrix.length; i++ ){
+			for( int j=0; j<matrix[0].length; j++ ){
+				System.out.print( Character.toString(matrix[i][j].red) + Character.toString(matrix[i][j].green) + " " );
+			
+				if( j==matrix[0].length-1 )
+					System.out.println( ' ' );
+			}
+		}
+
+		System.out.println( "" );
+		
+		for( int i=0; i<matrix.length; i++ ){
+			for( int j=0; j<matrix[0].length; j++ ){
+				matrix[i][j] = new Pixel(Integer.toString(i).charAt(0), Integer.toString(j).charAt(0), '0', '0' );
+			}
+		}
+
+		rotateMatrix3( matrix );
+		
+		System.out.println( "" );
+		System.out.println( "[Matrix Rotated by Method 3]");
 
 		for( int i=0; i<matrix.length; i++ ){
 			for( int j=0; j<matrix[0].length; j++ ){
